43d6592ac1ad53389aba01131cf473e52182d2c5
barte <barte@192.168.0.32>
2016-08-18 20:28:13 +0200

    Dodano formularz

--- init/dbConnect.php
+++ init/dbConnect.php
@@ -1,22 +1,22 @@
 <?php
-//Połączenie z bazą danych
 
+//Połączenie z bazą danych
 //    $user = "u848159436_mcadm";
 //    $psw = "MCode1234";
 //    $dbName = "u848159436_mcpiz";
 //    $host = "mysql.hostinger.pl";
 //    
-    $user = "u463957321_666";
-    $psw = "witchery11!";
-    $dbName = "u463957321_666";
-    $host = "mysql.hostinger.pl";
-    try {
-        $db = new PDO('mysql:host=localhost; dbname=pizzeria', 'root', '');
+$user = "u463957321_666";
+$psw = "witchery11!";
+$dbName = "u463957321_666";
+$host = "mysql.hostinger.pl";
+try {
+    $db = new PDO('mysql:host=localhost; dbname=pizzeria', 'root', '');
 //        $db = new PDO("mysql:host=$host;dbname=$dbName",$user,$psw);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        echo 'lol';
-    } catch (Exception $ex) {
-        echo 'Błąd połączenia z db';
-        echo $ex;
-    }
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo 'lol';
+} catch (Exception $ex) {
+    echo 'Błąd połączenia z db';
+    echo $ex;
+}
