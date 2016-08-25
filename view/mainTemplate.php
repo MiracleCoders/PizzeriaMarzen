1e87e64b49c357fc49e62e9a983833579fabbb6c
barte <barte@192.168.0.32>
2016-08-25 22:30:49 +0200

    1. Dodano nowy szablon strony 2. Dodano css'y 3. Dodano obrazki

---
+++ view/mainTemplate.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./styles/style.css">
+        <title></title>
+    </head>
+
+    <body>
+        <div id="mainWrapper">
+            <div id="bg">
+                <div class="mainHeader">
+
+                    <div class="col-12">
+                        <div class="mainLogo">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="mainMenu">
+                    <div class="col-12">
+                        <ul>
+                            <li><a class="active" href="#home">Strona główna</a></li>
+
+                            <li class="dropdown">
+                                <a href="#" class="dropbtn">Menu</a>
+                                <div class="dropdown-content">
+                                    <a href="#">Pizza</a>
+                                    <a href="#">Fast food</a>
+                                    <a href="#">Sałatki</a>
+                                </div>
+                            </li>
+
+                            <li class="dropdown">
+                                <a href="#" class="dropbtn">Administracja</a>
+                                <div class="dropdown-content">
+                                    <a href="#">Zarządzanie produktami</a>
+                                    <a href="#">Zarządzanie pracownikami</a>
+                                    <a href="#">Zarządzanie promocjami</a>
+                                    <a href="#">Opcje strony</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="mainContent">
+                    <div class="col-12">
+                        MAIN CONTENT
+                    </div
+                </div>
+
+                <div class="mainFooter">
+                    <div class="col-12">
+                        FOOTER
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </body>
+
+</html>
+
+
+<?php
